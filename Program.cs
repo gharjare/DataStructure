9ce6db81b67fb8c62636fa9b161179ddf81193c2
gharjare <gharjarechetan@gmail.com>
2023-02-16 16:26:54 +0530

    [chetan gharjare] prime number

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using DataStructureAlgorithm1;
+using System.Collections;
 
 namespace DataStrctureAlgorithm1
 { 
@@ -7,9 +8,17 @@ namespace DataStrctureAlgorithm1
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Datastructure algorithm");
-            Anagram anagram = new Anagram();
-            anagram.Display();
-            
+            Console.WriteLine("Enter starting point : ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Ending Point : ");
+            int end = Convert.ToInt32(Console.ReadLine());
+            ArrayList primeNumbers = Prime.FindPrimeNumbers(start, end);
+            Console.WriteLine($"\nAll the prime numbers between {start} and {end} are :");
+            foreach (int prime in primeNumbers)
+            {
+                Console.WriteLine(prime);
+            }
+
 
         }
     }
