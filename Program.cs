84fb5a13865c7da907e06dc0e14618e31973f4fc
gharjare <gharjarechetan@gmail.com>
2023-02-16 16:16:15 +0530

    [chetan gharjare] merge sort

--- Program.cs
+++ Program.cs
@@ -7,9 +7,13 @@ namespace DataStrctureAlgorithm1
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Datastructure algorithm");
-            Bubble sort = new Bubble();
-            sort.Display();
-
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            Console.WriteLine("Given Array");
+            Merge.printArray(arr);
+            Merge ob = new Merge();
+            ob.sort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\nSorted array");
+           Merge.printArray(arr);
 
 
         }
