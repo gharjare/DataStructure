b929d6a70387ba1b98e9fb5f1993536528422a85
gharjare <gharjarechetan@gmail.com>
2023-02-16 15:48:06 +0530

    [chetan gharjare] initial

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStrctureAlgorithm1
+{ 
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Datastructure algorithm");
+
+        }
+    }
+}
