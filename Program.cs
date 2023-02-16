4abec5b9974bcc03e7e5206e86618d1c675b80f5
gharjare <gharjarechetan@gmail.com>
2023-02-16 16:00:18 +0530

    [chetan gharjare] insertion sort

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿namespace DataStrctureAlgorithm1
+﻿using DataStructureAlgorithm1;
+
+namespace DataStrctureAlgorithm1
 { 
     class program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Datastructure algorithm");
-
+            Insertion sort = new Insertion();
+            sort.Display();
         }
     }
 }
