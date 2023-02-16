93dd6f607e268cd6b04a7ebd522bb9a1352df855
gharjare <gharjarechetan@gmail.com>
2023-02-16 16:22:31 +0530

    [chetan gharjare] Anagram detection

--- Program.cs
+++ Program.cs
@@ -7,14 +7,9 @@ namespace DataStrctureAlgorithm1
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Datastructure algorithm");
-            int[] arr = { 12, 11, 13, 5, 6, 7 };
-            Console.WriteLine("Given Array");
-            Merge.printArray(arr);
-            Merge ob = new Merge();
-            ob.sort(arr, 0, arr.Length - 1);
-            Console.WriteLine("\nSorted array");
-           Merge.printArray(arr);
-
+            Anagram anagram = new Anagram();
+            anagram.Display();
+            
 
         }
     }
